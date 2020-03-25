398038e555d4867f2eccf8eaad9121a83e5a160a
Nahara <44853730+nahara1@users.noreply.github.com>
2020-03-24 21:20:09 -0400

    Update mongoSN.py

--- CRUD/mongoSN.py
+++ CRUD/mongoSN.py
@@ -7,19 +7,19 @@ import pymongo
 
 client = MongoClient('localhost', 27017)
 db = client['recipes_db']
-collection_recipe = db['recipes']
+recipes = db['recipes']
 #GET request w/ database table fields specified
 
 #Need to install requests package for python
 #easy_install requests
-import requests, json, ast, itertools
+import requests, json
 
 # Set the request parameters
 '''
 Use sysparm_fields=field_name => field_name is the field you want to read
 separate fields using commas in case you want to read more than one field from a table
 '''
-url = 'https://emplkasperpsu2.service-now.com/api/now/table/x_snc_brewing440_recipe?sysparm_fields=recipe_name&displayvariables=true&sysparm_action=getKeys'
+url = 'https://emplkasperpsu2.service-now.com/api/now/table/x_snc_brewing440_recipe?sysparm_fields=sys_id%2Crecipe_name%2Ctype%2Cstyle%2Cog%2Cfg%2Cabv%2Cibu%2Cwater_volume%2Cwater_temperature%2Cgrain_bill%2Cboiling_duration%2Cyeast&sysparm_limit=100'
 
 #Use IST440 for both user and pwd
 user = 'IST440'
@@ -40,14 +40,14 @@ print()
 print("This is a json dictionary: ", data, "Type: ", type(data))
 
 
-# No we need to get the list of key value pairs from our dict
+# Now we need to get the list of key value pairs from our dict
 print()
 recipe_names_pairs = data['result']
 print("This is a list: ", recipe_names_pairs)
 print()
 
 # Check type
-print("Recipe_names_pairs is of the list type and can now be used to create documents in mongddb: " , type(recipe_names_pairs))
+print("Recipe_names_pairs is of the list type and can now be used to create documents in mongddb: ", type(recipe_names_pairs))
 print()
 #db.collection_recipe.insert(recipe_names_pairs)
 
@@ -59,9 +59,8 @@ for doc in recipe_names_pairs:
     try:
         # insert into db collection
         # print("Inserting ",  doc, " into db...")
-        message = "Inserting ",  doc, " into db..."
-        print(message)
-        db.collection_recipe.insert_one(doc)
+        message = "Inserting  into db..."
+        db.recipes.insert_one(doc)
     except pymongo.errors.DuplicateKeyError:
         # skip document because it already exists in the local db collection
         continue
